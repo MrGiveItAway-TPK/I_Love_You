37f1e299099b476c17f3f3e43228fbf6cb086087
Munes Bani Fawaz <100308806+MrGiveItAway-TPK@users.noreply.github.com>
2022-11-15 17:02:14 +0300

    heart-timing-commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var aText = new Array("What would I do without your smart mouth?", "Drawing me in, and you kicking me out", "You got my head spinning, no kidding, I can't pin you down", "What's going on in that beautiful mind?", "I'm on your magical mystery ride", "And I'm so dizzy, don't know what hit me, but I'll be alright", "My head's underwater", "But I'm breathing fine", "You're crazy and I'm out of my mind", "'Cause all...", "'Cause all of me", "Loves all of you", "Love your curves and all your edges", "All your perfect imperfections", "Give your all to me", "I'll give my all to you", "You're my end and my beginning", "Even when I lose I'm winning", "How many times do I have to tell you", "Even when you're crying you're beautiful too?", "The world is beating you down, I'm around through every mood", "You're my downfall, you're my muse", "My worst distraction, my rhythm and blues", "I can't stop singing, it's ringing in my head for you", "My head's underwater", "But I'm breathing fine", "You're crazy and I'm out of my mind", "'Cause all of me", "Loves all of you", "Love your curves and all your edges", "All your perfect imperfections", "Give your all to me", "I'll give my all to you", "You're my end and my beginning", "Even when I lose I'm winning", "'Cause I give you all...", "'Cause all of me", "Loves all of you", "Love your curves and all your edges", "All your perfect imperfections", "Give your all to me", "I'll give my all to you", "You're my end and my beginning", "Even when I lose I'm winning", "'Cause I give you all of me", "And you give me all of you", "'Cause I give you all of me", "And you give me all of you, oh");
-var iSpeed = 100;
+var iSpeed = 150;
 var iIndex = 0;
 var iArrLength = aText[0].length;
 var iScrollAt = 20;
@@ -35,7 +35,7 @@ function typewriter() {
 		iIndex++;
 		if(iIndex != aText.length) {
 			iArrLength = aText[iIndex].length;
-			setTimeout("typewriter()", 500);
+			setTimeout("typewriter()", 1000);
 		}
 	} else {
 		setTimeout("typewriter()", iSpeed);
